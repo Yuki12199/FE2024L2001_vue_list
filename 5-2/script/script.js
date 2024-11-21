2c89f5f9eafd81e2dcab4c94074bfd8d1e76c975
Yuki12199 <2301218@s.asojuku.ac.jp>
2024-11-21 03:04:29 +0000

    itemを追加

--- 5-2/script/script.js
+++ 5-2/script/script.js
@@ -2,11 +2,7 @@ new Vue({
   el: '#app',
   data() {
     return {
-      items: [
-        { id: 1, label: "子" },
-        { id: 2, label: "丑" },
-        { id: 3, label: "寅" }
-      ]
+      
     };
   }
 });
