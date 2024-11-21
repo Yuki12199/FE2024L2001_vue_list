c6429bf6ec39e28af14f448be2c88a1820f5b1e5
Yuki12199 <2301218@s.asojuku.ac.jp>
2024-11-21 03:09:21 +0000

    追加

--- 5-2/script/script.js
+++ 5-2/script/script.js
@@ -2,7 +2,19 @@ new Vue({
   el: '#app',
   data() {
     return {
-      
+      menus: [{
+        label: 'TOP',
+        path: './index.html'
+      },{
+        label: 'ABOUT',
+        path: './about.thml'
+      },{
+        label: 'SCHEDULE',
+        path: './shedule.html'
+      },{
+        label: 'CONTACT',
+        path: './contact.html'
+      }]
     };
   }
 });
